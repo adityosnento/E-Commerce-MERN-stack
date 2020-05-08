dcb7d338a5d332135049a08d93af26f3a5051965
Adityo Nento <adityosnento11@gmail.com>
2020-05-08 23:15:56 +0700

    [Adityo S. Nento] - Create Upload Product Page

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -7,7 +7,7 @@ import {
 function Footer() {
     return (
         <footer style={{ textAlign: 'center', padding: '1rem' }}>
-            By Adityo S. Nento   <SmileTwoTone />
+            By Adityo S. Nento :   <SmileTwoTone />
 
         </footer>
     )
